fix loss bug, broadcasting annoying

src/model/loss.py
@@ -208,44 +208,34 @@ class BundleLoss(nn.Module):
         shape_gt: torch.Tensor,
         trans_gt: torch.Tensor,
 
-        joint_cam_gt: torch.Tensor,
-        joint_cam_pred: torch.Tensor,
         joint_rel_gt: torch.Tensor,
         joint_rel_pred: torch.Tensor,
+        verts_rel_gt: torch.Tensor,
+        verts_rel_pred: torch.Tensor,
 
         mano_valid: torch.Tensor,
         joint_valid: torch.Tensor,
-
-        focal: torch.Tensor,
-        princpt: torch.Tensor,
     ):
         loss_theta = self.mse(pose_pred, pose_gt) # [b,t,d]
         loss_theta = torch.mean(loss_theta * mano_valid[..., None])
         loss_shape = self.mse(shape_pred, shape_gt) # [b,t,d]
         loss_shape = torch.mean(loss_shape * mano_valid[..., None])
 
+        loss_verts = self.l1(verts_rel_pred, verts_rel_gt) # [b,t,v,d]
+        loss_verts = torch.mean(loss_verts * mano_valid[..., None, None])
+
         if self.supervise_global:
             loss_joint_root = self.l1(trans_pred, trans_gt) # [b,t,d]
-            loss_joint_root = torch.mean(loss_joint_root * joint_valid[:, :, 0])
+            loss_joint_root = torch.mean(loss_joint_root * joint_valid[:, :, :1])
 
             loss_joint_rel = self.l1(joint_rel_pred, joint_rel_gt) # [b,t,j,d]
             loss_joint_rel = torch.mean(loss_joint_rel * joint_valid[..., None])
 
-            joint_proj_pred = self.proj_x3d(
-                joint_cam_pred, focal=focal, princpt=princpt
-            )
-            joint_proj_gt = self.proj_x3d(
-                joint_cam_gt, focal=focal, princpt=princpt
-            )
-            loss_proj = self.l1(joint_proj_pred, joint_proj_gt) # [b,t,j,d]
-            loss_proj = torch.mean(loss_proj * joint_valid[..., None])
-
-            loss_joint = self.glo * (loss_joint_root + loss_proj) + self.rel * loss_joint_rel
+            loss_joint = self.glo * loss_joint_root + self.rel * loss_joint_rel
 
             sub_state = {
                 "loss_joint_root": loss_joint_root.detach(),
                 "loss_joint_rel": loss_joint_rel.detach(),
-                "loss_proj": loss_proj.detach(),
             }
         else:
             loss_joint_rel = self.l1(joint_rel_pred, joint_rel_gt) # [b,t,j,d]
@@ -253,13 +243,14 @@ class BundleLoss(nn.Module):
 
             sub_state = {}
 
-        loss = loss_theta + loss_shape + loss_joint
+        loss = loss_theta + loss_shape + loss_verts + loss_joint
 
         return (
             loss,
             {
                 "loss_theta": loss_theta.detach(),
                 "loss_shape": loss_shape.detach(),
+                "loss_verts": loss_verts.detach(),
                 "loss_joint": loss_joint.detach(),
             }
             | sub_state,

src/model/net.py
@@ -323,17 +323,7 @@ class PoseNet(nn.Module):
             "(b t) j d -> b t j d", b=batch_size, j=njoint_hand
         )
 
-        if self.norm_by_hand:
-            norm_scale = rearrange(
-                self.get_hand_norm_scale(joints) * 1e3, "(b t) -> b t", b=batch_size
-            )
-            return (
-                joint_rel / norm_scale[:, None, None],
-                verts_rel / norm_scale[:, None, None],
-                norm_scale,
-            )
-        else:
-            return joint_rel, verts_rel, torch.ones_like(joint_rel[:, :, 0])
+        return joint_rel, verts_rel
 
     def forward(self, batch):
         # 1. forward
@@ -346,18 +336,20 @@ class PoseNet(nn.Module):
         )
 
         # 2. loss, fk
-        joint_rel_pred, verts_rel_pred, norm_scale_pred = self.mano_to_pose(
+        joint_rel_pred, verts_rel_pred = self.mano_to_pose(
             pose_pred, shape_pred
         )
+        norm_scale_pred = self.get_hand_norm_scale(joint_rel_pred)
         joint_cam_pred = joint_rel_pred + trans_pred[:, :, None]
         verts_cam_pred = verts_rel_pred + trans_pred[:, :, None]
 
         with torch.no_grad():
-            _, verts_rel_gt, norm_scale_gt = self.mano_to_pose(
+            _, verts_rel_gt = self.mano_to_pose(
                 batch["mano_pose"],
                 batch["mano_shape"],
             )
-        joint_cam_gt = batch["joint_cam"] / norm_scale_gt[..., None, None]
+        norm_scale_gt = self.get_hand_norm_scale(batch["joint_cam"])
+        joint_cam_gt = batch["joint_cam"]
         joint_rel_gt = joint_cam_gt - joint_cam_gt[:, :, :1]
         verts_cam_gt = verts_rel_gt + joint_cam_gt[:, :, :1]
 
@@ -367,25 +359,22 @@ class PoseNet(nn.Module):
             trans_pred,
             batch["mano_pose"],
             batch["mano_shape"],
-            joint_cam_gt[:, :, 0],
-            joint_cam_gt,
-            joint_cam_pred,
+            joint_cam_gt[:, :, 0] / norm_scale_gt[..., None],
+
             joint_rel_gt,
             joint_rel_pred,
+            verts_rel_gt,
+            verts_rel_pred,
+
             batch["mano_valid"],
             batch["joint_valid"],
-            batch["focal"],
-            batch["princpt"],
         )
 
-        joint_cam_gt = joint_cam_gt * norm_scale_gt[..., None, None]
-        joint_rel_gt = joint_rel_gt * norm_scale_gt[..., None, None]
-        verts_cam_gt = verts_cam_gt * norm_scale_gt[..., None, None]
-        verts_rel_gt = verts_rel_gt * norm_scale_gt[..., None, None]
-        joint_cam_pred = joint_cam_pred * norm_scale_gt[..., None, None]
-        joint_rel_pred = joint_rel_pred * norm_scale_gt[..., None, None]
-        verts_cam_pred = verts_cam_pred * norm_scale_gt[..., None, None]
-        verts_rel_pred = verts_rel_pred * norm_scale_gt[..., None, None]
+        norm_trans_factor = (norm_scale_gt / norm_scale_pred)[..., None, None]
+        joint_cam_pred = joint_cam_pred * norm_trans_factor
+        joint_rel_pred = joint_rel_pred * norm_trans_factor
+        verts_cam_pred = verts_cam_pred * norm_trans_factor
+        verts_rel_pred = verts_rel_pred * norm_trans_factor
 
         # 3. micro metric
         metric_state = self.metric_meter(