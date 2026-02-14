fix: metric computation for stage2

script/train.py
@@ -289,7 +289,7 @@ def val(
 
         output = net(batch)
 
-        joint_cam_gt = batch["joint_cam"]
+        joint_cam_gt = batch["joint_cam"][:, -1:]
         joint_rel_gt = joint_cam_gt - joint_cam_gt[:, :, :1]
         verts_cam_gt = output["result"]["verts_cam_gt"]
         verts_rel_gt = verts_cam_gt - joint_cam_gt[:, :, :1]
@@ -299,11 +299,11 @@ def val(
         verts_cam_pred = output["result"]["verts_cam_pred"]
         verts_rel_pred = verts_cam_pred - joint_cam_pred[:, :, :1]
 
-        joint_valid = batch["joint_valid"]
-        mano_valid = batch["mano_valid"]
+        joint_valid = batch["joint_valid"][:, -1:]
+        mano_valid = batch["mano_valid"][:, -1:]
         if "norm_idx" in output["result"]:
             norm_idx = output["result"]["norm_idx"]
-            norm_valid = torch.all(batch["joint_valid"][:, :, norm_idx] > 0.5, dim=-1).float()
+            norm_valid = torch.all(batch["joint_valid"][:, -1:, norm_idx] > 0.5, dim=-1).float()
         else:
             norm_valid = torch.ones(joint_valid.shape[:2], device=joint_valid.device)
 

src/utils/metric.py
@@ -65,6 +65,8 @@ def compute_mpvpe_stats(
     # 2. 计算欧式距离
     # [B, T, V, 3] -> [B, T, V]
     error_per_vertex = torch.norm(pred - gt, p=2, dim=-1)
+    # [B, T]
+    error_per_vertex = torch.mean(error_per_vertex, dim=-1)
 
     # 3. 处理 Mask
     # 原逻辑中 metrics_accum[3] += verts_error[mask_v].numel()
@@ -76,14 +78,9 @@ def compute_mpvpe_stats(
     mask_bool = mask > 0.5  # [B, T]
 
     if mask_bool.any():
-        # 利用 PyTorch 的高级索引:
-        # error_per_vertex[mask_bool] 会选出 mask 为 True 的那些帧的所有顶点
-        # 结果形状为 [N_valid_frames, V]
-        valid_errors = error_per_vertex[mask_bool]
-
-        batch_total_error = valid_errors.sum()
-        batch_total_count = valid_errors.numel()  # 自动计算 N_valid_frames * V
-        return batch_total_error, torch.tensor(batch_total_count, device=pred.device)
+        batch_total_error = (error_per_vertex * mask).sum()
+        batch_total_count = mask_bool.sum()
+        return batch_total_error, batch_total_count
     else:
         return torch.tensor(0.0, device=pred.device), torch.tensor(
             0.0, device=pred.device